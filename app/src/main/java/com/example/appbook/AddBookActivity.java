8fca82f439dc580e4f8d9cb10fc4e98244440832
Joana Freitas <joanafreitas13@gmail.com>
2023-01-08 12:04:07 +0100

    Camara fotos en addBook

--- app/src/main/java/com/example/appbook/AddBookActivity.java
+++ app/src/main/java/com/example/appbook/AddBookActivity.java
@@ -2,10 +2,16 @@ package com.example.appbook;
 
 import static com.example.appbook.db.Constants.DATABASE_NAME;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +23,24 @@ import com.mapbox.maps.MapView;
 
 public class AddBookActivity extends AppCompatActivity {
 
+    Button btnCamera; //boton para abrir la camera
+    ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
+
+        btnCamera = findViewById(R.id.camara_add_book_button);
+        imageView = findViewById(R.id.book_add_image);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camera();
+            }
+        });
     }
 
     //boton AÑADIR
@@ -58,7 +78,21 @@ public class AddBookActivity extends AppCompatActivity {
     }
     public void cancelButton(View view) {
         onBackPressed();
-
     }
 
+    private void camera() {
+        Intent takeFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takeFotoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeFotoIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
 }
